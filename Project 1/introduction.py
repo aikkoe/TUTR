5b9a50db19e2bdcc97c7afc4aa63823069dc180d
Povrik <aikkoe@ya.ru>
2025-04-14 12:36:32 +0300

    Сделал первый набросок текстовой игры

---
+++ Project 1/introduction.py
@@ -0,0 +1,46 @@
+import random
+
+
+print('####################')
+print('#     RPG GAME     #')
+print('####################')
+# ИНФОРМАЦИЯ О ИГРОКЕ
+USER_NAME = input('\nПриветсвую! Введите своё имя: ')
+USER_HP = 100  # healt
+USER_MP = 100  # mana
+USER_INVENT = []  # inventory
+
+# СПИСОК ЛОКАЦИЙ
+LOCATIONS = ['Anor-Londo', 'Jopa-boli', 'Bolotishko']
+
+# СПИСОК СОБЫЙТИЙ
+event = ['Засада!', 'Найден предмет!', 'Ты встретил костёр и отдыхаешь.']
+
+
+def game():
+    while True:
+        # ТЕЛО ИГРЫ
+        print(
+            f'\n{USER_NAME}, перед тобой следующие места: \n1.{LOCATIONS[0]} \n2.{LOCATIONS[1]} \n3.{LOCATIONS[2]}')
+
+        while True:
+            user_choice = input('\nВыбери куда отправишься(1, 2 или 3): ')
+            if user_choice in ['1', '2', '3']:
+                break
+            else:
+                print('Введи число 1, 2 или 3!')
+
+        print()
+        print(f'Ты прибыл в {LOCATIONS[int(user_choice)-1]}')
+        print()
+
+        print(random.choice(event))
+        print()
+
+        user_choice_v2 = input('Хотите продолжить?(да или нет): ')
+        if user_choice_v2.lower() == 'нет':
+            print('\nПокойся с миром...')
+            break
+
+
+game()
