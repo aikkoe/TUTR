0d1f0c70812ec2add1cf95dd351705baf5a4b118
Povrik <aikkoe@ya.ru>
2025-04-17 20:45:11 +0300

    1. Модернизировал БС; 2. Попробовал изменение цвета текста

--- Project 1/introduction.py
+++ Project 1/introduction.py
@@ -2,6 +2,13 @@ import random
 import copy
 import time
 import sys
+import re
+from colorama import init, Fore, Style
+
+
+#  Инициализирую библеотеку
+#  Это нужно, так как модуль colorama работает в терминале для вывода цветного текста
+init(autoreset=True)
 
 
 # функция медленного вывода текста
@@ -54,14 +61,14 @@ def game(user_hp, user_invent):
         if event == events[1]:  # выпадение рандомного предмета
             item = random.choice(items)
             user_invent.append(item)
-            slow_print(f'Ты нашёл {item}!\n')
-
-        if event == events[0]:
+            slow_print(f'Ты нашёл:')
+            print(Fore.YELLOW + item)
+            print()
+        elif event == events[0]:  # инвент с боем
             random_enemy = random.choice(enemies)
             enemy_copy = copy.deepcopy(random_enemy)
-            user_hp = fight(enemy_copy, user_hp)
-
-        if event == events[2]:
+            user_hp, user_invent = fight(enemy_copy, user_hp, user_invent)
+        elif event == events[2]:  # ивент коста
             user_hp = 100
             slow_print('Ты восстановил здоровье!\n')
 
@@ -104,10 +111,10 @@ enemy_king = {
 enemies = [enemy_zombie, enemy_solder, enemy_king]
 
 
-def fight(enemy, user_hp):  # БОЕВАЯ СИСТЕМА
+def fight(enemy, user_hp, user_invent):  # БОЕВАЯ СИСТЕМА
     slow_print(f"Ты встретил {enemy['name']}!")
     while enemy['hp'] > 0 and user_hp > 0:
-        slow_print(f'Он приближается к тебе...')
+        slow_print(f'Он приближается к тебе...\n')
         slow_print(f'Ты 1.Атакуешь или 2.Бежишь?')
         user_choice = input()
         if user_choice == '1':
@@ -115,7 +122,14 @@ def fight(enemy, user_hp):  # БОЕВАЯ СИСТЕМА
             enemy['hp'] -= 15
             slow_print(f"\nУ врага осталось {enemy['hp']} HP")
             slow_print(f"\nВраг атакует тебя!")
-            user_hp -= enemy['damage']
+            # подсчет кол-ва урона врага
+            enemy_damage = enemy['damage'] + (random.randint(-5, 5))
+            user_hp -= enemy_damage
+
+            # крит - если урон врага превышает базовый урон на 3
+            if enemy_damage > enemy['damage'] + 3:
+                slow_print(f'Враг нанёс критический удар!')
+
             slow_print(f"\nУ тебя осталось {user_hp} HP\n")
         elif user_choice == '2':
             slow_print(f"\nТы убегаешь от {enemy['name']}!")
@@ -125,14 +139,18 @@ def fight(enemy, user_hp):  # БОЕВАЯ СИСТЕМА
 
     if user_hp <= 0:
         slow_print('...YOU DIED...')
-    elif user_choice == '2':
+        exit()
+    elif user_choice == '2':  # type: ignore
         slow_print(f"\nТы теряешь 10 HP во время побега!")
         user_hp -= 10
         slow_print(f"\nУ тебя осталось {user_hp} HP\n")
     else:
+        item = random.choice(items)
+        user_invent.append(item)
+        slow_print(f'Ты получил с врага {item}!\n')
         slow_print(f"{enemy['name']} повержен!")
 
-    return user_hp
+    return user_hp, user_invent
 
 
 game(hp, invent)
---
+++ study.py
@@ -0,0 +1,6 @@
+from colorama import init, Fore
+
+init(autoreset=True)
+print(Fore.RED + "Это красный текст")
+print(Fore.GREEN + "Это зелёный текст")
+print("Это нормальный текст.")
